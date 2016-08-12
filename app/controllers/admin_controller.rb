18698b76d70b578bf7bd32fcfe3cdd018b14b62b
Andrés Da Viá <andresi.davia@gmail.com>
2016-08-12 10:27:40 -0500

    Add admin authorization methods

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,5 +1,15 @@
 class AdminController < ApplicationController
+  before_action :authorize_admin!
+
   def index
     @total_orders = Order.count
+  end
+
+  private
+
+  def authorize_admin!
+    unless current_user.is_admin?
+      redirect_to root_path, notice: "You must be an admin to do that."
+    end
   end
 end
